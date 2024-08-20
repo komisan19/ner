056985ea7b952312dd9de860a09098fc30904585
komisan19 <18901496+komisan19@users.noreply.github.com>
2024-08-20 10:16:24 +0900

    add relay func

--- main.go
+++ main.go
@@ -1,25 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log/slog"
+	"os"
 	"strings"
 
 	"github.com/nbd-wtf/go-nostr"
-	"github.com/nbd-wtf/go-nostr/nip19"
 )
 
 func maskKey(key string) string {
 	return strings.Repeat("*", 4)
 }
 
-func main() {
-	sk := nostr.GeneratePrivateKey()
-	pk, _ := nostr.GetPublicKey(sk)
-	nsec, _ := nip19.EncodePrivateKey(sk)
-	npub, _ := nip19.EncodePublicKey(pk)
+var (
+	urls = [2]string{"wss://relay-jp.nostr.wirednet.jp", "wss://relay.nostr.wirednet.jp"}
+	sk   string
+)
 
-	fmt.Println("sk: ", maskKey(sk))
-	fmt.Println("pk: ", maskKey(pk))
-	fmt.Println("nsec: ", maskKey(nsec))
-	fmt.Println("npub: ", maskKey(npub))
+func init() {
+	sk = os.Getenv("NOSTR_SK")
+}
+
+func main() {
+	publishRelay(urls)
+}
+
+func publishRelay(urls [2]string) {
+	pub, err := nostr.GetPublicKey(sk)
+	if err != nil {
+		slog.Warn("error: ", err)
+		return
+	}
+
+	ev := nostr.Event{
+		PubKey:    pub,
+		CreatedAt: nostr.Now(),
+		Kind:      nostr.KindTextNote,
+		Tags:      nil,
+		Content:   "Hello World for coudflare",
+	}
+
+	ev.Sign(sk)
+
+	ctx := context.TODO()
+
+	for _, url := range urls {
+		relay, err := nostr.RelayConnect(ctx, url)
+		if err != nil {
+			slog.Warn("error: ", err)
+			continue
+		}
+		if err := relay.Publish(ctx, ev); err != nil {
+			slog.Warn("error: ", err)
+			continue
+		}
+
+		slog.Warn("sucess: ", url)
+	}
 }
